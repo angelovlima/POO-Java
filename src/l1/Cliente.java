1424e3faeda1a38e7cbef2ca89eb19cc551f8034
Angelo Lima <angelovictor2001@hotmail.com>
2020-04-18 22:04:34 -0300

    teste 2

--- src/l1/Cliente.java
+++ src/l1/Cliente.java
@@ -3,5 +3,5 @@ package l1;
 public class Cliente {
 	private String nome;
 	
-
+	
 }
--- src/l1/Inicio.java
+++ src/l1/Inicio.java
@@ -1,10 +1,17 @@
 package l1;
 
+import java.util.Scanner;
+
 public class Inicio {
 
 	public static void main(String[] args) {
+		int x;
+		Scanner sc = new Scanner(System.in);
+		x = sc.nextInt();
 		
-
+		System.out.println(x);
+		
 	}
 
 }
+
